51ef0fe027bb7fd1750dcf0571a47dc7979f1278
ylarhris <y.larhrissi.2001@gmail.com>
2023-03-15 11:27:20 +0100

    add some functions to js file

--- script.js
+++ script.js
@@ -16,11 +16,30 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-document.querySelector('.check').addEventListener('click',function(){
-    const guess = document.querySelector('.guess').value;
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+
+
+const guessing = function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
     if(!guess){
         document.querySelector('.message').textContent = '⛔️ No number!'; 
     }
-    else
-        console.log(guess);
-});
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    }
+    else if(guess > secretNumber){
+        document.querySelector('.message').textContent = '📈 Too high!';
+        score--;
+        document.querySelector('.score').textContent = score; 
+    }
+    else if(guess < secretNumber){
+        document.querySelector('.message').textContent = '📉 Too low!';
+        score--;
+        document.querySelector('.score').textContent = score; 
+    }
+};
+document.querySelector('.check').addEventListener('click',guessing);
